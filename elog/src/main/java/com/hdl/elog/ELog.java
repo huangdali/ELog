0751c31d4183db6dd8440717641d6ccba1323fd5
huangdali <137722697@qq.com>
2017-06-01 10:19:32 +0800

    新增打印日志到文件，Elog.file(fileName,msg);

--- elog/src/main/java/com/hdl/elog/ELog.java
+++ elog/src/main/java/com/hdl/elog/ELog.java
@@ -1,6 +1,14 @@
 package com.hdl.elog;
 
+import android.os.Environment;
 import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * E级别错误日志打印工具，带定位功能（输出调用处类型名、方法名、行，点击可追踪到）
@@ -36,8 +44,7 @@ public class ELog {
     /**
      * 打印错误信息
      *
-     * @param msg
-     *  不推荐使用，可以用自己的方法
+     * @param msg 不推荐使用，可以用自己的方法
      */
     public static void e(String msg) {
         if (!isDebug) {
@@ -49,8 +56,7 @@ public class ELog {
     /**
      * 打印错误信息
      *
-     * @param msg
-     *  不推荐使用，可以用自己的方法
+     * @param msg 不推荐使用，可以用自己的方法
      */
     public static void e(String tag, String msg) {
         if (!isDebug) {
@@ -180,6 +186,18 @@ public class ELog {
     }
 
     /**
+     * 打印错误信息
+     *
+     * @param msg
+     */
+    public static void file(String fileName, String msg) {
+        if (!isDebug) {
+            return;
+        }
+        printMsgToFile(getStackTrace(), fileName, msg);
+    }
+
+    /**
      * 获取StackTraceElement对象-----当前调用ELog.e()处的类信息（类名、方法名、行等）
      *
      * @return
@@ -250,6 +268,49 @@ public class ELog {
                 .append(") ");
         String tag = author + "-" + sb.toString();
         Log.e(tag, msg);
+    }
+
+    /**
+     * 打印日志到文件
+     *
+     * @param element  当前调用ELog.e()处的类信息（类名、方法名、行等）
+     * @param fileName
+     * @param msg
+     */
+    private static void printMsgToFile(StackTraceElement element, String fileName, String msg) {
+        StringBuilder sb = new StringBuilder();
+        String className = element.getFileName();
+        sb.append(element.getMethodName())
+                .append(" (").append(className).append(":")
+                .append(element.getLineNumber())
+                .append(") ");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:SSS");
+        String content = "-----------------------   " +
+                format.format(new Date(System.currentTimeMillis()))
+                + "   -----------------------\n" + sb.toString() + "\n" +
+                msg + "\n\n\n";
+        File dir = new File(Environment.getExternalStorageDirectory().getPath() + "/ELog/");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(dir.getPath()+"/" + fileName, true);
+            fos.write(content.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
 }
